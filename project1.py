6fe5a47e6270e0a37de8e3d9d864a6906769bf7d
haiyenn_tr <t.haiyen0430@gmail.com>
2019-06-26 02:21:02 +0700

    done level1 ,level2

--- project1.py
+++ project1.py
@@ -1,17 +1,20 @@
 from turtle import *
 import turtle
+import time
+import queue
 import math
-images=["pacman-r 1.gif","bg.gif","ghost 1.gif"]
-for image in images:
-    turtle.register_shape(image)
-wm =turtle.Screen()
-wm.bgpic("bg.gif")
-wm.title("PACMAN")
-wm.setup(750,550)
+
+def _Screen():
+    wm =turtle.Screen()
+    wm.bgpic("bg.gif")
+    wm.bgcolor("black")
+    wm.title("PACMAN")
+    wm.setup(750,550)
 
 class Wall(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
+        self.hideturtle()
         self.shape("square")
         self.color("blue")
         self.shapesize(1)
@@ -20,6 +23,7 @@ class Wall(turtle.Turtle):
 class Coin(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
+        self.hideturtle()
         self.shape("circle")
         self.color("yellow")
         self.shapesize(0.5)
@@ -27,50 +31,99 @@ class Coin(turtle.Turtle):
         self.speed(0)
 class Player(turtle.Turtle):
     def __init__(self):
-        #turtle.addshape("pacman2.gif")
         turtle.Turtle.__init__(self)
+        self.hideturtle()
         self.shape("pacman-r 1.gif")
-        self.color("white")
         self.shapesize(0.7)
         self.penup()
         self.speed(0)
 class Ghost(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
+        self.hideturtle()
         self.shape("ghost 1.gif")
         self.penup()
         self.speed(0)
+class Score(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.hideturtle()
+        self.penup()
+        self.goto(-240, -260)
+        self.color("yellow")
+        self.write("Score:", move=False, align="center", font=("Arial", 36, "normal"))
+        self.speed(0)
 
-levels=[]
-
-def setup_level(level,height,width):
+def setup_level(level,height,width,pos1):
     for y in range(len(level)):
         for x in range(len(level[y])):
-
             char = level[y][x]
-            screen_x =-130+(x*24)
-            screen_y =70-(y*24)
-
+            screen_x = -130+(x*24)
+            screen_y = 70-(y*24)
             if char == "1":
+                wall.showturtle()
                 wall.goto(screen_x,screen_y)
                 wall.stamp()
                 walls.append((screen_x,screen_y))
-            elif char =="2":
+            elif char == "2":
+                coin.showturtle()
                 coin.goto(screen_x,screen_y)
                 coin.stamp()
                 coins.append((screen_x,screen_y))
+            elif char == "3":
+                ghost.showturtle()
+                ghost.goto(screen_x,screen_y)
+                ghost.stamp()
+                ghosts.append((screen_x,screen_y))
             elif char == "P":
+                player.showturtle()
                 player.goto(screen_x,screen_y)
+                pos1.append((screen_x,screen_y))
 
+class White(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(1)
+        self.penup()
+        self.speed(0)
 
-def readFILE(arr,path):
-    pos = []
+def Move(maze,x,y,add):
+    white=White()
+    for i in range(len(add)):
+        if(add[i]=="L"):
+            player.goto(x-24,y)
+            white.goto(x,y)
+            white.stamp()
+            time.sleep(0.5)
+            x=x-24
+        if (add[i] == "R"):
+            player.goto(x + 24, y)
+            white.goto(x, y)
+            white.stamp()
+            time.sleep(0.5)
+            x = x + 24
+        if (add[i] == "U"):
+            player.goto(x, y+24)
+            white.goto(x, y)
+            white.stamp()
+            time.sleep(0.5)
+            y = y + 24
+        if (add[i] == "D"):
+            player.goto(x , y - 24)
+            white.goto(x, y)
+            white.stamp()
+            time.sleep(0.5)
+            y = y - 24
+
+def readFILE(arr,path,pos,coins1):
     global height, width
     file = open(path, "r")
     hw = []
     hw.append(file.readline().split())
-    height = int(hw[0][0])
-    width = int(hw[0][1])
+    width = int(hw[0][0])
+    height = int(hw[0][1])
     l = 1
     for line in file:
         if l <= width:
@@ -83,28 +136,326 @@ def readFILE(arr,path):
         for x in range(len(arr[y])):
             if int(pos[0][0]) == y and int(pos[0][1]) == x:
                 arr[y][x] = "P"
-
+    for y in range(len(arr)):
+        for x in range(len(arr[y])):
+            if arr[y][x] == "2":
+                coins1.append((y,x))
     file.close()
     return
 
+def updateScore():
+    global scores
+    s = turtle.Turtle()
+    s.hideturtle()
+    s.penup()
+    s.goto(-110,-260)
+    while True:
+        score =Score()
+        s.write(str(scores), move=False, align="center", font=("Arial", 36, "normal"))
+        s.color("yellow")
+        scores +=1
+        time.sleep(0.5)
+        s.clear()
+
+def printMaze(maze, path=""):
+    global i, j, start, start
+    for index in range(len(maze)):
+        for x, pos in enumerate(maze[index]):
+            if pos == "P":
+                start = x
+                start_ = index
+
+    i = start
+    j = start_
+    pos = set()
+    for move in path:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+        pos.add((j, i))
+
+    for j, row in enumerate(maze):
+        for i, col in enumerate(row):
+            if (j, i) in pos:
+                maze[j][i]="+"
+                print("+ ", end="")
+            else:
+                print(col + " ", end="")
+        print()
+
+
+def valid(maze, moves):
+    global i, j, start, start_
+    for index in range(len(maze)):
+         for x, pos in enumerate(maze[index]):
+             if pos == "P":
+                 start = x
+                 start_ = index
+
+    i = start
+    j = start_
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+        if not (0 <= i < len(maze[0]) and 0 <= j < len(maze)):
+            return False
+        elif (maze[j][i] == "1"):
+            return False
+        elif (maze[j][i] == "3"):
+            return False
+
+    return True
+
+
+def findEnd(maze, moves):
+    global i,j,start,start_
+    for index in range(len(maze)):
+        for x, pos in enumerate(maze[index]):
+            if pos == "P":
+                start = x
+                start_ = index
+
+    i = start
+    j = start_
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+    if maze[j][i] == "2":
+        #print("Found: " + moves)
+        #printMaze(maze, moves)
+        return True
+
+    return False
+
+def level1_map1(level,pos,coin1):
+    global add
+    readFILE(level, "input1.txt",pos,coins1)
+    setup_level(level, height, width,pos1)
+    while not findEnd(level, add):  # nếu chưa chạm đích thì tiếp tục
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(level, put):
+                nums.put(put)
+    Move(level, int(pos1[0][0]), int(pos1[0][1]),add)
+
+def level2_map1(level1,pos,coins1):
+    global add2
+    readFILE(level1, "input1_2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1_2)
+    while not findEnd(level1, add2):  # nếu chưa chạm đích thì tiếp tục
+        add2 = nums2.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add2 + j
+            if valid(level1, put):
+                nums2.put(put)
+    Move(level1, int(pos1_2[0][0]), int(pos1_2[0][1]),add2)
+
+def level1_map2(level1,pos,coins1):
+    global add
+    readFILE(level1, "input2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1)
+    while not findEnd(level1, add):  # nếu chưa chạm đích thì tiếp tục
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(level1, put):
+                nums.put(put)
+    Move(level1, int(pos1[0][0]), int(pos1[0][1]),add)
+
+def level2_map2(level1,pos,coins1):
+    global add2
+    readFILE(level1, "input2_2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1_2)
+    while not findEnd(level1, add2):  # nếu chưa chạm đích thì tiếp tục
+        add2 = nums2.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add2 + j
+            if valid(level1, put):
+                nums2.put(put)
+    Move(level1, int(pos1_2[0][0]), int(pos1_2[0][1]),add2)
+
+def level1_map3(level1,pos,coins1):
+    global add
+    readFILE(level1, "input3.txt",pos,coins1)
+    setup_level(level1, height, width,pos1)
+    while not findEnd(level1, add):  # nếu chưa chạm đích thì tiếp tục
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(level1, put):
+                nums.put(put)
+    Move(level1, int(pos1[0][0]), int(pos1[0][1]),add)
+
+def level2_map3(level1,pos,coins1):
+    global add2
+    readFILE(level1, "input3_2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1_2)
+    while not findEnd(level1, add2):  # nếu chưa chạm đích thì tiếp tục
+        add2 = nums2.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add2 + j
+            if valid(level1, put):
+                nums2.put(put)
+    Move(level1, int(pos1_2[0][0]), int(pos1_2[0][1]),add2)
+def level1_map4(level1,pos,coins1):
+    global add
+    readFILE(level1, "input4.txt",pos,coins1)
+    setup_level(level1, height, width,pos1)
+    while not findEnd(level1, add):  # nếu chưa chạm đích thì tiếp tục
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(level1, put):
+                nums.put(put)
+    Move(level1, int(pos1[0][0]), int(pos1[0][1]),add)
+
+def level2_map4(level1,pos,coins1):
+    global add2
+    readFILE(level1, "input4_2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1_2)
+    while not findEnd(level1, add2):  # nếu chưa chạm đích thì tiếp tục
+        add2 = nums2.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add2 + j
+            if valid(level1, put):
+                nums2.put(put)
+    Move(level1, int(pos1_2[0][0]), int(pos1_2[0][1]),add2)
+
+def level1_map5(level1,pos,coins1):
+    global add
+    readFILE(level1, "input5.txt",pos,coins1)
+    setup_level(level1, height, width,pos1)
+    while not findEnd(level1, add):  # nếu chưa chạm đích thì tiếp tục
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(level1, put):
+                nums.put(put)
+    Move(level1, int(pos1[0][0]), int(pos1[0][1]),add)
+
+def level2_map5(level1,pos,coins1):
+    global add2
+    readFILE(level1, "input5_2.txt",pos,coins1)
+    setup_level(level1, height, width,pos1_2)
+    while not findEnd(level1, add2):  # nếu chưa chạm đích thì tiếp tục
+        add2 = nums2.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add2 + j
+            if valid(level1, put):
+                nums2.put(put)
+    Move(level1, int(pos1_2[0][0]), int(pos1_2[0][1]),add2)
 
 if __name__=="__main__":
+    _Screen()
+    images = ["pacman-r 1.gif", "bg.gif", "ghost 1.gif"]
+    for image in images:
+        turtle.register_shape(image)
     map = int(Screen().numinput("Choose map","Enter map(from 1 to 5)",1,1,5))
-    lev = int(Screen().numinput("Choose level", "Enter level(from 1 to 4)", 1, 1, 4))
-    levels = []
     level1 = []
-    walls = []
-    coins = []
+    level2 = []
+    walls = [] #vector cua wall
+    coins = [] #vector cua coin
+    ghosts =[]  #vector cua ghost
+    coins1 = [] #vi tri cua coin level1
+    coins2= [] #vi tri cua coin level2
+
+    pos = [] # vi tri vector cua player level1
+    pos_2 = [] #vi tri vector cua player level2
+
+    pos1 = []  # vi tri cua player level1 trong mang
+    pos1_2 =[]  # vi tri cua player level2 trong mang
+    scores = 0
     height = 0
     width = 0
+    start = 0
+    start_ = 0
     wall = Wall()
     coin = Coin()
     player = Player()
-    if map == 1 and lev == 1:
-        readFILE(level1,"input.txt")
-        setup_level(level1, height, width)
-    elif map == 2 and lev == 1:
-        readFILE(level1,"input2.txt")
-        setup_level(level1,height,width)
-
+    ghost=Ghost()
+    nums = queue.Queue()#cua level1
+    nums.put("")
+    nums2 = queue.Queue() #cua level2
+    nums2.put("")
+    add = "" #duong di cua level 1
+    add2 ="" #duong di cua level 2
+    if map == 1:
+        level1_map1(level1,pos,coins1)
+        time.sleep(1)
+        Screen().clear()
+        _Screen()
+        coin = Coin()
+        wall = Wall()
+        player = Player()
+        ghost = Ghost()
+        level2_map1(level2,pos_2,coins2)
+    elif map == 2:
+        level1_map2(level1,pos,coins1)
+        time.sleep(1)
+        Screen().clear()
+        _Screen()
+        coin = Coin()
+        wall = Wall()
+        player = Player()
+        ghost = Ghost()
+        level2_map2(level2, pos_2, coins2)
+    elif map == 3:
+        level1_map3(level1,pos,coins1)
+        time.sleep(1)
+        Screen().clear()
+        _Screen()
+        coin = Coin()
+        wall = Wall()
+        player = Player()
+        ghost = Ghost()
+        level2_map3(level2, pos_2, coins2)
+    elif map == 4:
+        level1_map4(level1,pos,coins1)
+        time.sleep(1)
+        Screen().clear()
+        _Screen()
+        coin = Coin()
+        wall = Wall()
+        player = Player()
+        ghost = Ghost()
+        level2_map4(level2, pos_2, coins2)
+    elif map == 5:
+        level1_map5(level1,pos,coins1)
+        time.sleep(1)
+        Screen().clear()
+        _Screen()
+        coin = Coin()
+        wall = Wall()
+        player = Player()
+        ghost = Ghost()
+        level2_map5(level2, pos_2, coins2)
 turtle.done()
